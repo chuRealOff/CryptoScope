8a30e5d61d4155e7c8f5f39cbfd0eaef9bbf1a54
Dmitry Churilov <98746743+chuRealOff@users.noreply.github.com>
2024-05-02 19:10:59 +0300

    Initial Commit

---
+++ CryptoScope/CryptoScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoScopeApp.swift
+//  CryptoScope
+//
+//  Created by CHURILOV DMITRIY on 02.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
